707a839a8fe0f10bee73b9104ee0971136755b18
kehops <kehops>
2022-11-23 12:32:17 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+
+from two import display
+
+
+#from my_main_package import some_main_script
+#from my_main_package.sub_package import my_subscript
+
+# some_main_script.main_report()
+
+# my_subscript.sub_report()
+
+# call the package: my_main_package from mymodule.my_func
+
+
+row1 = ['','','']
+row2 = ['','','']
+row3 = ['','','']
+
+
+
+
+
+if __name__ == '__main__':
+    #run the script
+    display(row1,row2,row3)
+
+
+
---
+++ two.py
@@ -0,0 +1,12 @@
+'''
+    display ft will serve as a canvas
+'''
+
+def display(row1,row2,row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+
+
+
